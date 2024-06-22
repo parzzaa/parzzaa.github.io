c7bd4cf1a8d44ea18e424fa2ff9d1100c3ccd998
PARZA <parzaps4@gmai.com>
2024-06-22 14:52:35 +0330

    add telegram webapp

--- main.js
+++ main.js
@@ -2,6 +2,23 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import WebApp from '@twa-dev/sdk'
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Handle the main button click event
+    WebApp.onEvent('mainButtonClicked', function() {
+        WebApp.close();
+    });
+
+    // Customize the main button
+    WebApp.MainButton.text = "Close";
+    WebApp.MainButton.show();
+
+    // Retrieve user data and display it
+    const user = WebApp.initDataUnsafe.user;
+    document.getElementById('user-info').innerText = `Hello, ${user.first_name}!`;
+});
 
 document.querySelector('#app').innerHTML = `
   <div>
